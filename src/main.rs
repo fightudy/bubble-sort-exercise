aa014d5e10e6f50eada2ee591b94a29690e905c3
xgzh <xgzh0312@163.com>
2023-11-09 18:10:28 +0800

    [+]bubble sort implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::cmp::PartialOrd;
+
 fn main() {
-    println!("Hello, world!");
+    let raw_data = vec!(4, 6, 32, 7, 2, 7, 4, 2);
+    println!("i32类型排序前：{:?}", raw_data);
+    let sorted_data = bubble_sort(raw_data);
+    println!("i32类型排序后：{:?}", sorted_data);
+
+    let raw_data_t = vec!(String::from("yuiyiyu"), String::from("ddddd"),
+                          String::from("asdasd"), String::from("aaddd"),
+                          String::from("xcczas"), String::from("ewqeq"));
+    println!("泛型排序前：{:?}", raw_data_t);
+    let sorted_data_t = bubble_sort_t(raw_data_t);
+    println!("泛型排序后：{:?}", sorted_data_t);
+}
+
+fn bubble_sort(mut data_list: Vec<i32>) -> Vec<i32>{
+    for i in 0..data_list.len() {
+        for j in i..data_list.len() {
+            if data_list[i] > data_list[j] {
+                data_list.swap(i, j);
+            }
+        }
+    }
+    data_list
+}
+
+fn bubble_sort_t<T: PartialOrd>(mut data_list: Vec<T>) -> Vec<T>{
+    for i in 0..data_list.len() {
+        for j in i..data_list.len() {
+            if data_list[i] > data_list[j] {
+                data_list.swap(i, j);
+            }
+        }
+    }
+    data_list
 }
